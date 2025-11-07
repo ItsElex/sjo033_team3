029896df794f48fa7424513e1b442704f0671d5b
ItsElex <142209057+ItsElex@users.noreply.github.com>
2025-11-07 19:01:24 +0100

    fixed pkg, created map in gazebo

--- src/multi_room/setup.py
+++ src/multi_room/setup.py
@@ -1,35 +1,40 @@
+from setuptools import setup
 import os
 from glob import glob
-from setuptools import setup
 
 package_name = 'multi_room'
 
 setup(
     name=package_name,
-    version='0.0.1',
+    version='0.0.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        # Install launch files
-        (os.path.join('share', package_name, 'launch'),
-            glob(os.path.join('launch', '*.launch.py'))),
-        # Install world files
-        (os.path.join('share', package_name, 'worlds'),
-            glob(os.path.join('worlds', '*'))),
         # Install config files
         (os.path.join('share', package_name, 'config'),
-            glob(os.path.join('config', '*.yaml'))),
+            glob(os.path.join('config', '*'))),
+        # Install launch files
+        (os.path.join('share', package_name, 'launch'),
+            glob(os.path.join('launch', '*.py'))),
+        # Install worlds
+        (os.path.join('share', package_name, 'worlds'),
+            glob(os.path.join('worlds', '*.sdf'))),
+        # Install rviz
+        (os.path.join('share', package_name, 'rviz'),
+            glob(os.path.join('rviz', '*.rviz'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='elex',
     maintainer_email='elex@example.com',
-    description='Multi-room robot navigation',
+    description='Multi-room robot simulation',
     license='Apache-2.0',
+    tests_require=['pytest'],
     entry_points={
-        'console_scripts': [],
+        'console_scripts': [
+        ],
     },
 )
 
